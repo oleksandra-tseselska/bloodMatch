b445b7fdd8fac13834711be6fb9d0202d20f4b94
Oleksandra Tseselska <oleksandra.tseselska@accenture.com>
2023-07-24 14:29:55 +0300

    initial commit

---
+++ src/main/java/com/bloodMatch/BloodMatchApplication.java
@@ -0,0 +1,13 @@
+package com.bloodMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bloodMatch/BloodMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloodMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
